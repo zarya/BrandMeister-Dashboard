c6ef60e21ace845b0a951d2b483b2bb8c54d421c
Rudy Hardeman <zarya@gigafreak.net>
2015-12-09 17:54:13 +0100

    Activated masters page

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -32,6 +32,7 @@
                 <div class="span3 statbox purple" onTablet="span6" onDesktop="span3">
                     <div class="number" id="count_master"></div>
                     <div class="title">Masters</div>
+                      <a href="index.php?page=masters">Full report</a>
                     <div class="footer">
                     </div>
                 </div>
