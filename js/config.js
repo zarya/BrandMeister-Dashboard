aab863bcee88a8b9b246e3b9233de88c0eaeb4e3
Rudy Hardeman <zarya@gigafreak.net>
2016-01-16 00:57:17 +0100

    Things

--- js/config.js
+++ js/config.js
@@ -2,3 +2,8 @@ function saveSettings(){
   localStorage.setItem('datetime', $('#datetime').is(':checked'));
   window.location.reload();
 }
+
+function loadSettings() {
+    if (localStorage.getItem('todos') != undefined)
+        config['timedate'] = localStorage.getItem('todos');
+}
