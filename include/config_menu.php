8867e149ceae077b3ac77eab9cc63ef187467033
Rudy Hardeman <zarya@gigafreak.net>
2016-01-16 01:28:35 +0100

    Made config tabs

--- include/config_menu.php
+++ include/config_menu.php
@@ -4,9 +4,21 @@
             <h3>Settings</h3>
         </div>
         <div class="modal-body">
-            <p>Here settings can be configured...</p>
-            <div class="checkbox">
-              <label><input type="checkbox" id="datetime"> Display date/time per QSO</label>
+            <div class="tabbable">
+                <ul class="nav nav-tabs">
+                    <li class="active"><a href="#config_tab_lh" data-toggle="tab">LH</a></li>
+                    <li><a href="#tab2" data-toggle="tab">Section 2</a></li>
+                </ul>
+                <div class="tab-content">
+                    <div class="tab-pane active" id="config_tab_lh">
+                        <div class="checkbox">
+                          <label><input type="checkbox" id="datetime"> Display date/time per QSO</label>
+                        </div>
+                    </div>
+                    <div class="tab-pane" id="tab2">
+                        <p>Here settings can be configured...</p>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="modal-footer">
