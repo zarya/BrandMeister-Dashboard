c3e476898d13c0ff73d706cd29d6bc9d83e4910f
Rudy Hardeman <zarya@gigafreak.net>
2016-01-22 09:33:01 +0100

    Added links to master overview

--- js/masters.js
+++ js/masters.js
@@ -1,36 +1,38 @@
+var table = [];
+
+function updateRepeaterListCallback(master,host) {
+  return function(data) {
+    var device = {};
+    device['Country'] = CountryImage(getCountry(data['network']))+" "+getCountry(data['network']).toUpperCase();
+    device['Number'] = data['network'];
+    device['Version'] = data['version'];
+    device['Links'] = '<a role="button" class="btn btn-xs btn-primary" target="_blank" href="http://' + host + '/status/status.htm">Status</a>&nbsp;' + '<a role="button" class="btn btn-xs btn-info" target="_blank" href="http://' + host + '/status/list.htm">List</a>&nbsp;';
+    table.push(device);
+    document.getElementById("json").innerHTML = ConvertJsonToTable(table, 'jsonTable', "table table-striped table-bordered bootstrap-datatable datatable", 'Bla');
+    $('#jsonTable').dataTable({
+        "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span12'i><'span12 center'p>>",
+        "sPaginationType": "bootstrap",
+        "oLanguage": {
+        "sLengthMenu": "_MENU_ records per page"
+        },
+        "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ]
+    } );
 
 function updateRepeaterList()
 {
-  var table = [];
+  table = [];
   for (var number in servers) {
-    var location = 'http://' + servers[number] + '/status/';
-    $.getJSON(location + 'system.php?callback=?',
-      function(data)
-      {
-        var device = {};
-        device['Country'] = CountryImage(getCountry(data['network']))+" "+getCountry(data['network']).toUpperCase();
-        device['Number'] = data['network'];
-        device['Version'] = data['version'];
-        table.push(device);
-        document.getElementById("json").innerHTML = ConvertJsonToTable(table, 'jsonTable', "table table-striped table-bordered bootstrap-datatable datatable", 'Bla');
-        $('#jsonTable').dataTable({
-            "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span12'i><'span12 center'p>>",
-            "sPaginationType": "bootstrap",
-            "oLanguage": {
-            "sLengthMenu": "_MENU_ records per page"
-            },
-            "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ]
-        } );
-      }
-    ); 
+    $.getJSON('http://' + servers[number] + '/status/system.php?callback=?', updateRepeaterListCallback(number,servers[number]));
   }
 }
+
 function getCountry(number)
 {
   var value = String(number).substring(0,3);
   if (countries.hasOwnProperty(value))
     return countries[value];
 }
+
 function CountryImage(country){
   if (country != null)
     return '<img src="images/flags/png/' + country + '.png" \>';
