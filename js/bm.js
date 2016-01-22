30095f102e401f8cb58728483d50efc75b41112d
Rudy Hardeman <zarya@gigafreak.net>
2016-01-22 15:45:23 +0100

    Code cleanup

--- js/bm.js
+++ js/bm.js
@@ -248,3 +248,197 @@ if (retina()) {
 
     }
 });
+
+var max_queue = 5;
+var table = [];
+var last = [];
+var socket = io.connect('http://home.gigafreak.net:5001');
+
+socket.on('connect', function () {
+  var table = [];
+  var last = [];
+  if (params['country']) params['filter'] = params['country'];
+  socket.emit('subscribe',{topic:'filter/'+params['filter']+'/'+params['repeater']});
+  socket.on('mqtt', function (msg) {
+    var lastraw = JSON.parse(msg.payload);
+    if (params['filter']) { 
+      if (lastraw['Master'].toString().substring(0,3) != params['filter'] )
+        return;
+    }
+    if (params['repeater']) { 
+      if (lastraw['ContextID'].toString() != params['repeater'] )
+        return;
+    }
+    
+
+    options = "";
+    if (lastraw['LinkName'] == "D-Extra Link" || lastraw['LinkName'] == "DCS Link") {
+        options = "<img src=images/avc/icon_D-STAR.png \>";
+    }else{
+        options = "<img src=images/avc/icon_DMR_w.png \>";
+    }
+    group = "";
+    if (lastraw['CallTypes'].indexOf("Group") > -1) {
+      group = '<img src="images/batch/users.png" \>';
+    }else if(lastraw['CallTypes'].indexOf("Voice") > -1) {
+      group = '<img src="images/batch/user.png" \>';
+    }
+
+    if (lastraw['SourceCall'] == '')
+      lastraw['SourceCall'] = getGroupName(lastraw['SourceID']);
+
+    var scountry = getCountry(lastraw['SourceID']);
+    var rcountry = getCountry(lastraw['ContextID']);
+    var dcountry = getCountry(lastraw['DestinationID']);
+    var ref = "";
+    
+    if (lastraw['SourceName'] == undefined || lastraw['SourceName'] == null || lastraw['SourceName'] == "") {
+      var Source = CountryImage(scountry) +' ' + lastraw['SourceCall'] + ' (' + lastraw['SourceID'] + ')';
+    } else {
+      var Source = CountryImage(scountry) +' '+lastraw['SourceCall'] + ' [' + lastraw['SourceName'] + '] (' + lastraw['SourceID'] + ')' 
+    }
+
+    if (lastraw['ReflectorID'] != undefined)
+      ref = getGroupName(lastraw['ReflectorID']) + ' (' + lastraw['ReflectorID'] + ')';
+    if (config['datetime']) {
+      var timestamp = lastraw['Start'];
+      var date = new Date(timestamp * 1000);
+      var datetime = date.format('yyyy-mm-dd HH:MM:ss');
+    }
+    else
+      var datetime = timeSince(lastraw['Start'])
+
+    var LossCount = "";
+    if (lastraw['TotalCount']!=0 && lastraw['TotalCount'] != "undefined") {
+      var _percent = Math.round(lastraw['LossCount'] / lastraw['TotalCount'] * 10000)/100;
+      LossCount = _percent + "% (" + lastraw['LossCount'] + "/" + lastraw['TotalCount'] + ")";
+    }
+    var entry = {
+      'Time': datetime, 
+      'Link name': lastraw['LinkName'],
+      'My call': Source,
+      'Source':CountryImage(rcountry) + ' ' + lastraw['LinkCall'] + ' (' + lastraw['ContextID'] + ')',
+      'Destination': group + ' ' + CountryImage(dcountry) + ' ' + lastraw['DestinationCall'] + getGroupName(lastraw['DestinationID']) +' (' + lastraw['DestinationID'] + ')',
+      'Reflector': ref
+    };
+    var index = functiontofindIndexByKeyValue(last,'SessionID', lastraw['SessionID']);
+    if (index != null) {
+      table[index] = entry;
+      last[index] = lastraw;
+    } else {
+      if (msg.topic == 'LH-Startup') {
+        last.push(JSON.parse(msg.payload));
+        table.push(entry);
+      } else {
+        last.unshift(JSON.parse(msg.payload));
+        table.unshift(entry);
+      }
+    }
+    table = table.slice(0,parseInt(max_queue));
+    last = last.slice(0,parseInt(max_queue));
+    document.getElementById("json").innerHTML = ConvertJsonToTable(table, 'jsonTable', "table table-striped table-bordered bootstrap-datatable datatable", 'Bla');
+  });
+});
+
+function getCellDuration(value)
+{
+  if (isNaN(value))
+    return '';
+  var seconds = Number(value);
+  if (seconds >= 60)
+  {
+    var minutes = Math.floor(seconds / 60);
+    seconds -= minutes * 60;
+    var delimiter = (seconds < 10) ? ':0' : ':';
+    return minutes + delimiter + seconds.toFixed(0);
+  }
+  return seconds.toFixed(0);
+}
+
+function getCountry(number)
+{
+  var value = String(number).substring(0,3);
+  if (countries.hasOwnProperty(value))
+    return countries[value];
+}
+function CountryImage(country){
+  if (country != null)
+    return '<img src="images/flags/png/' + country + '.png" \>';
+  else
+    return '';
+}
+function sMeter(rssi) {
+    if (rssi > -63) return '<img src="images/indicator/4.png" \> S9+10dB';
+    if (rssi > -73) return '<img src="images/indicator/4.png" \> S9';
+    if (rssi > -79) return '<img src="images/indicator/3.png" \> S8';
+    if (rssi > -85) return '<img src="images/indicator/3.png" \> S7';
+    if (rssi > -91) return '<img src="images/indicator/2.png" \> S6';
+    if (rssi > -97) return '<img src="images/indicator/2.png" \> S5';
+    if (rssi > -103) return '<img src="images/indicator/1.png" \> S4';
+    if (rssi > -109) return '<img src="images/indicator/1.png" \> S3';
+    if (rssi > -115) return '<img src="images/indicator/0.png" \> S2';
+    if (rssi > -121) return '<img src="images/indicator/0.png" \> S1';
+    return '<img src="images/indicator/0.png" \> S0';
+}
+function TSimage(ts) {
+  if (ts > 0)
+    return '<img src="images/avc/icon_TS' + ts + '.png" \>';
+  else
+    return '<img src="images/avc/icon_TS.png" \>';
+}
+
+function timeSince(date) {
+    date = date * 1000
+    var seconds = Math.floor((new Date() - date)/1000);
+
+    var interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) {
+        return interval + " Years";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+        return interval + " Months";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+        return interval + " Days";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+        return interval + " Hours";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+        return interval + " Minutes";
+    }
+    return Math.floor(seconds) + " Seconds";
+}
+
+function functiontofindIndexByKeyValue(arraytosearch, key, valuetosearch) {
+  for (var i = 0; i < arraytosearch.length; i++) {
+    if (arraytosearch[i][key] == valuetosearch) {
+      return i;
+    }
+  }
+  return null;
+}
+
+var params = function() {
+    function urldecode(str) {
+        return decodeURIComponent((str+'').replace(/\+/g, '%20'));
+    }
+
+    function transformToAssocArray( prmstr ) {
+        var params = {};
+        var prmarr = prmstr.split("&");
+        for ( var i = 0; i < prmarr.length; i++) {
+            var tmparr = prmarr[i].split("=");
+            params[tmparr[0]] = urldecode(tmparr[1]);
+        }
+        return params;
+    }
+
+    var prmstr = window.location.search.substr(1);
+    return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+}();
