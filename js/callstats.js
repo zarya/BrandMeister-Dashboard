7a92575b9968267df99e6319716600c0087d8191
Rudy Hardeman <zarya@gigafreak.net>
2016-02-25 17:02:46 +0100

    Fixed resetting the group dropdown

--- js/callstats.js
+++ js/callstats.js
@@ -26,7 +26,7 @@ $('#group-by-tg').on('switchChange.bootstrapSwitch', function() {
     delete params['reflectors'];
     params['destination'] = 1;
     $("#group-by-ref").bootstrapSwitch('state', false);
-    $("#group-list").selectedIndex = 0;
+    $("#group-list").reset();
   }
   else
   {
@@ -42,7 +42,7 @@ $('#group-by-ref').on('switchChange.bootstrapSwitch', function() {
     delete params['destination'];
     params['reflectors'] = 1;
     $("#group-by-tg").bootstrapSwitch('state', false);
-    $("#group-list").selectedIndex = 0;
+    $("#group-list").reset();
   }
   else
   {
