1af2c720bebbe9bc6fa948f6e870ff5d2830a3af
Rudy Hardeman <zarya@gigafreak.net>
2016-03-02 22:01:11 +0100

    Added call stats in seconds

--- js/callstats.js
+++ js/callstats.js
@@ -60,12 +60,21 @@ function RenderGraph()
   var days = 2;
   if (params['repeater']) days = 15;
   if (params['destination'])
+  {
     var data_url = 'https://bm-lastheard.pi9noz.ampr.org/stats/?groupdst=1&days='+days+'&totalcount=5'+filters+'&callback=?'
+    var data_url_sec = 'https://bm-lastheard.pi9noz.ampr.org/stats/?groupdst=1&days='+days+'&totalcount=5'+filters+'&seconds=1&callback=?'
+  }
   else if (params['reflectors'])
+  {
     var data_url = 'https://bm-lastheard.pi9noz.ampr.org/stats/?groupref=1&days='+days+'&totalcount=5'+filters+'&callback=?'
+    var data_url_sec = 'https://bm-lastheard.pi9noz.ampr.org/stats/?groupref=1&days='+days+'&totalcount=5'+filters+'&seconds=1&callback=?'
+  }
   else
+  {
     var data_url = 'https://bm-lastheard.pi9noz.ampr.org/stats/?days=30'+filters+'&totalcount=5&callback=?'
-     
+    var data_url_sec = 'https://bm-lastheard.pi9noz.ampr.org/stats/?days=30'+filters+'&totalcount=5&seconds=1&callback=?'
+  }
+  
   $.getJSON(data_url, function (jsondata) {
     data = [{type: 'area', name: 'Total'}];
 
@@ -122,6 +131,97 @@ function RenderGraph()
       yAxis: {
         title: {
           text: 'QSOs'
+        },
+        min: 0
+      },
+      legend: {
+        enabled: true 
+      },
+      plotOptions: {
+        area: {
+          fillColor: {
+            linearGradient: {
+              x1: 0,
+              y1: 0,
+              x2: 0,
+              y2: 1
+            },
+            stops: [
+              [0, Highcharts.getOptions().colors[0]],
+              [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+            ]
+          },
+          marker: {
+            radius: 2
+          },
+          lineWidth: 1,
+          states: {
+            hover: {
+              lineWidth: 1
+            }
+          },
+          threshold: null
+        }
+      },
+      series: data
+    });
+  });
+  $.getJSON(data_url_sec, function (jsondata) {
+    data = [{type: 'area', name: 'Total'}];
+
+    //Generate total
+    qso = [];
+    destination = {};
+    for (index in jsondata)
+    {
+      if (jsondata[index]['qso'])
+      {
+        qso.push([parseInt(index), jsondata[index]['qso']]);
+      }
+      else if (jsondata[index][0]['destination'])
+      {
+        for(tgindex in jsondata[index])
+        {
+          if (destination[jsondata[index][tgindex]['destination']] == undefined) destination[jsondata[index][tgindex]['destination']] = [];
+          destination[jsondata[index][tgindex]['destination']].push([parseInt(index), jsondata[index][tgindex]['qso']]);
+        }
+      }
+    }
+    if (params['destination'] || params['reflectors'])
+    {
+      data = [];
+      for(index in destination)
+      { 
+        if (params['repeater'] == undefined && params['reflectors'] == undefined && ( index == 0 || index > 999 ) ) continue
+        if (params['reflectors'] && (index < 4000 || index > 5000 || index=="null")) continue
+        talkgroup = getGroupName(index,0)
+        if (talkgroup == "") talkgroup = index;
+        data.push({type: 'area', name: talkgroup,data: destination[index]});
+      }
+    } 
+    else
+      data[0]['data'] = qso;
+
+    console.log(data);
+
+    $('#container2').highcharts({
+      chart: {
+        zoomType: 'x',
+        type: 'area'
+      },
+      title: {
+        text: 'Seconds per Hour'
+      },
+      subtitle: {
+        text: document.ontouchstart === undefined ?
+          'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
+        },
+      xAxis: {
+        type: 'datetime'
+      },
+      yAxis: {
+        title: {
+          text: 'Seconds'
         },
         min: 0
       },
