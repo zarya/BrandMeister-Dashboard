7db7455930af2935fea2efa1caa2f9caef6a88d1
Rudy Hardeman <zarya@gigafreak.net>
2016-03-06 12:30:06 +0100

    Added get language override

--- include/lang.php
+++ include/lang.php
@@ -1,5 +1,6 @@
 <?php
 $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+if ($_GET['lang']) $lang = $_GET['lang'];
 switch ($lang){
     case "en":
         $string = file_get_contents("lang/en.json");
