5d4a09b1bf47a79f88430e1d5022fd102ab08a06
Rudy Hardeman <zarya@gigafreak.net>
2016-03-06 14:43:50 +0100

    Updated lanuage files for datatables

--- js/repeaters.js
+++ js/repeaters.js
@@ -27,6 +27,7 @@ function updateRepeaterListCallback(master) {
     $('#jsonTable').dataTable({
         "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span12'i><'span12 center'p>>",
         "sPaginationType": "bootstrap",
+        "language": php_lang['Table']['DataTables'],
         "oLanguage": {
           "sLengthMenu": "_MENU_ "+php_lang['Table']['records per page']
         },
