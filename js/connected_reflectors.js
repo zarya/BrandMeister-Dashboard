42b39bfd97cd06bad3f8b26f96b24d29084c8a1e
Rudy Hardeman <zarya@gigafreak.net>
2016-03-07 00:31:38 +0100

    trying to fix connected reflector page

--- js/connected_reflectors.js
+++ js/connected_reflectors.js
@@ -11,9 +11,13 @@ function updateRepeaterNames()
   };
   for (var number in servers) {
     var location = 'http://' + servers[number] + '/status/';
-    $.getJSON(location + 'list.php?callback=?',{format:'json'})
-      .done(function(data)
-      {
+    $.getJSON(location + 'list.php?callback=?',{format:'json'}, updateRepeaterNamesCallback(number))
+   ); 
+  }
+}
+
+function updateRepeaterNamesCallback(master) {
+  return function(data) {
         for (key in data)
         {
           var value = data[key];
@@ -21,44 +25,38 @@ function updateRepeaterNames()
           device[value['number']]['name'] = CountryImage(getCountry(value['number']))+" " + value['name'];
           device[value['number']]['country'] = getCountry(value['number']);
         }
-        t = setTimeout(updateReflectors, 100)
+        updateReflectors(master); 
       }
-   ); 
   }
 }
 
-function updateReflectors() {
-  clearTimeout(t);
-  console.log("updateReflectors");
+function updateReflectors(number) {
   var table = [];
-  for (var number in servers) {
-    var location = 'http://' + servers[number] + '/status/';
-    $.getJSON(location + 'status.php?callback=?',
-      function(data)
-      {
-        for (index in data) {
-          var value = data[index];
-          if (device[value['number']]) {
-            device[value['number']]['ref'] = value['values'][19];
-            if (device[value['number']]['ref'] > 0 && (params['search']==undefined || params['search']==device[value['number']]['country']))
-              entry  = {};
-              entry[php_lang['Connected reflectors']['Name']] = device[value['number']]['name'];
-              entry[php_lang['Connected reflectors']['Reflector']] = device[value['number']]['ref'];
-              table.push(entry);
-          }
+  var location = 'http://' + servers[number] + '/status/';
+  $.getJSON(location + 'status.php?callback=?',
+    function(data)
+    {
+      for (index in data) {
+        var value = data[index];
+        if (device[value['number']]) {
+          device[value['number']]['ref'] = value['values'][19];
+          if (device[value['number']]['ref'] > 0 && (params['search']==undefined || params['search']==device[value['number']]['country']))
+            entry  = {};
+            entry[php_lang['Connected reflectors']['Name']] = device[value['number']]['name'];
+            entry[php_lang['Connected reflectors']['Reflector']] = device[value['number']]['ref'];
+            table.push(entry);
         }
-        document.getElementById("json").innerHTML = ConvertJsonToTable(table, 'jsonTable', "table table-striped table-bordered bootstrap-datatable datatable", 'Bla');
-        $('#jsonTable').dataTable({
-          "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span12'i><'span12 center'p>>",
-          "sPaginationType": "bootstrap",
-          "language": php_lang['Table']['DataTables'],
-          "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, php_lang['Table']['All']] ]
-        });
       }
-    );
-  }
+      document.getElementById("json").innerHTML = ConvertJsonToTable(table, 'jsonTable', "table table-striped table-bordered bootstrap-datatable datatable", 'Bla');
+      $('#jsonTable').dataTable({
+        "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span12'i><'span12 center'p>>",
+        "sPaginationType": "bootstrap",
+        "language": php_lang['Table']['DataTables'],
+        "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, php_lang['Table']['All']] ]
+      });
+    }
+  );
 }
 
 updateRepeaterNames();
-setInterval(updateRepeaterNames, 100000);
 
