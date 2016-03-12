6df25a13ce07364f5e3e7266be62467e8fc38a75
Rudy Hardeman <zarya@gigafreak.net>
2016-03-12 11:22:14 +0100

    Updated repeater page improoved speed

--- js/repeaters.js
+++ js/repeaters.js
@@ -1,4 +1,4 @@
-var table = [];
+var dataTable;
 
 function updateRepeaterListCallback(master) {
   return function(data) {
@@ -6,40 +6,45 @@ function updateRepeaterListCallback(master) {
     {
       var value = data[key];
       if (value['link'] < 4) {
-        var device = {};
-        device[php_lang['Repeaters']['Number']] = CountryImage(getCountry(value['number']))+" "+value['number'];
-        device[php_lang['Repeaters']['Name']] = '<a href="'+baseurl+'index.php?page=lh&country=null&repeater='+value['number']+'&unique=1">' + value['name'] + '</a> <a href="'+baseurl+'index.php?page=callstats&repeater='+value['number']+'"><i class="icon-bar-chart"></i></a>';
-        device[php_lang['Repeaters']['Hardware']] = getRepeaterModel(value['hardware'],value['number']);
-        device[php_lang['Repeaters']['Firmware']] = value['firmware'];
-        device[php_lang['Repeaters']['TX']] = getFrequency(value['frequency1']);
-        device[php_lang['Repeaters']['RX']] = getFrequency(value['frequency2']);
-        device[php_lang['Repeaters']['CC']] = value['color'];
-        device[php_lang['Repeaters']['Status']] = getLinkDescription(value['link']);
-        device[php_lang['Repeaters']['Master']] = master;
+        device = [];
+        device[0] = CountryImage(getCountry(value['number']))+" "+value['number'];
+        device[1] = '<a href="'+baseurl+'index.php?page=lh&country=null&repeater='+value['number']+'&unique=1">' + value['name'] + '</a> <a href="'+baseurl+'index.php?page=callstats&repeater='+value['number']+'"><i class="icon-bar-chart"></i></a>';
+        device[2] = getRepeaterModel(value['hardware'],value['number']);
+        device[3] = value['firmware'];
+        device[4] = getFrequency(value['frequency1']);
+        device[5] = getFrequency(value['frequency2']);
+        device[6] = ""+value['color'];
+        device[7] = getLinkDescription(value['link']);
+        device[8] = master;
         if (params['search']) {
           if (params['search'] == getCountry(value['number']))
-            table.push(device);
-        } else
-        table.push(device);
+          {
+            dataTable.row.add(device).draw();
+          }
+        } 
+        else
+          dataTable.row.add(device).draw();
       }
     }
-    document.getElementById("json").innerHTML = ConvertJsonToTable(table, 'jsonTable', "table table-striped table-bordered bootstrap-datatable datatable", 'Bla');
-    $('#jsonTable').dataTable({
-        "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span12'i><'span12 center'p>>",
-        "sPaginationType": "bootstrap",
-        "language": php_lang['Table']['DataTables'],
-        "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, php_lang['Table']['All']] ]
-    } );
   }
 }
 
 function updateRepeaterList()
 {
-  table = [];
   for (var number in servers) {
     $.getJSON('http://' + servers[number] + '/status/list.php?callback=?', updateRepeaterListCallback(number)).fail(newAlertPopup('Error!','Master '+number+' not responding'));
   }
 }
+function initTable () {
+  return $('#jsonTable').DataTable({
+    "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span12'i><'span12 center'p>>",
+    "sPaginationType": "bootstrap",
+    "language": php_lang['Table']['DataTables'],
+    "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, php_lang['Table']['All']] ]
+  } );
+}
 
-updateRepeaterList();
-
+$(document).ready(function() {
+  dataTable = initTable();
+  updateRepeaterList();
+});
--- pages/repeaters.php
+++ pages/repeaters.php
@@ -8,7 +8,23 @@
             </ul>
 
 <div class="row-fluid">
-<div id="json"></div>
+<div id="json">
+<table id="jsonTable" class="table table-striped table-bordered bootstrap-datatable">
+  <thead>
+    <tr>
+      <th><?php echo $language['Repeaters']['Number'];?></th>
+      <th><?php echo $language['Repeaters']['Name'];?></th>
+      <th><?php echo $language['Repeaters']['Hardware'];?></th>
+      <th><?php echo $language['Repeaters']['Firmware'];?></th>
+      <th><?php echo $language['Repeaters']['TX'];?></th>
+      <th><?php echo $language['Repeaters']['RX'];?></th>
+      <th><?php echo $language['Repeaters']['CC'];?></th>
+      <th><?php echo $language['Repeaters']['Status'];?></th>
+      <th><?php echo $language['Repeaters']['Master'];?></th>
+    </tr>
+  </thead>
+</table>
+</div>
 </div>
     </div><!--/.fluid-container-->
     <script src="js/repeaters.js"></script>
