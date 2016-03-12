d785e528277db372eebf8d508ba628e0267ef4cc
Rudy Hardeman <zarya@gigafreak.net>
2016-03-12 23:29:22 +0100

    Updated dongle page

--- js/dongles.js
+++ js/dongles.js
@@ -1,4 +1,4 @@
-var table = [];
+var dataTable;
 
 function updateDongleListCallback(master) {
   return function(data) {
@@ -6,35 +6,41 @@ function updateDongleListCallback(master) {
     {
       var value = data[key];
       if (value['link'] == 4) {
-        var device = {};
-        device[php_lang['Hotspots']['Number']] = CountryImage(getCountry(value['number']))+" "+value['number'];
-        device[php_lang['Hotspots']['Name']] = '<a href="'+baseurl+'index.php?page=lh&country=null&repeater='+value['number']+'">' + value['name'] + '</a>';
-        device[php_lang['Hotspots']['Hardware']] = getRepeaterModel(value['hardware'],value['number']);
-        device[php_lang['Hotspots']['Firmware']] = value['firmware'];
-        device[php_lang['Hotspots']['TX']] = getFrequency(value['frequency1']);
-        device[php_lang['Hotspots']['RX']] = getFrequency(value['frequency2']);
-        device[php_lang['Hotspots']['CC']] = value['color'];
-        device[php_lang['Hotspots']['Status']] = getLinkDescription(value['link']);
-        device[php_lang['Hotspots']['Master']] = master; 
-        table.push(device);
+        var device = [];
+        device[0] = CountryImage(getCountry(value['number']))+" "+value['number'];
+        device[1] = '<a href="'+baseurl+'index.php?page=lh&country=null&repeater='+value['number']+'">' + value['name'] + '</a>';
+        device[2] = getRepeaterModel(value['hardware'],value['number']);
+        device[3] = value['firmware'];
+        device[4] = getFrequency(value['frequency1']);
+        device[5] = getFrequency(value['frequency2']);
+        device[6] = value['color'];
+        device[7] = getLinkDescription(value['link']);
+        device[8] = master; 
+        dataTable.row.add(device); 
       }
     }
-    document.getElementById("json").innerHTML = ConvertJsonToTable(table, 'jsonTable', "table table-striped table-bordered bootstrap-datatable datatable", 'Bla');
-    $('#jsonTable').dataTable({
-        "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span12'i><'span12 center'p>>",
-        "sPaginationType": "bootstrap",
-        "language": php_lang['Table']['DataTables'], 
-        "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, php_lang['Table']['All']] ]
-    } );
+    dataTable.draw();
   }
+}
+
+function initTable () {
+  return $('#jsonTable').DataTable({
+    "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span12'i><'span12 center'p>>",
+    "sPaginationType": "bootstrap",
+    "language": php_lang['Table']['DataTables'],
+    "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, php_lang['Table']['All']] ]
+  } );
 }
 
 function updateDongleList()
 {
-  table = [];
   for (var number in servers) {
     $.getJSON('http://' + servers[number] + '/status/list.php?callback=?', updateDongleListCallback(number)).fail(newAlertPopup('Error!','Master '+number+' not responding'));
   }
 }
 
-updateDongleList();
+$(document).ready(function() {
+  dataTable = initTable();
+  updateDongleList();
+});
+
--- pages/dongles.php
+++ pages/dongles.php
@@ -8,7 +8,23 @@
             </ul>
 
 <div class="row-fluid">
-                        <div id="json"></div>
+<div id="json">
+<table id="jsonTable" class="">
+  <thead>
+    <tr>
+      <th><?php echo $language['Hotspots']['Number'];?></th>
+      <th><?php echo $language['Hotspots']['Name'];?></th>
+      <th><?php echo $language['Hotspots']['Hardware'];?></th>
+      <th><?php echo $language['Hotspots']['Firmware'];?></th>
+      <th><?php echo $language['Hotspots']['TX'];?></th>
+      <th><?php echo $language['Hotspots']['RX'];?></th>
+      <th><?php echo $language['Hotspots']['CC'];?></th>
+      <th><?php echo $language['Hotspots']['Status'];?></th>
+      <th><?php echo $language['Hotspots']['Master'];?></th>
+    </tr>
+  </thead>
+</table>
+</div>
 </div>
     </div><!--/.fluid-container-->
     <script src="js/dongles.js"></script>
