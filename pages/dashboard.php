256739fa6db4c414d3e27f0e551d1290f55fb3e7
Rudy Hardeman <zarya@gigafreak.net>
2016-03-12 21:59:22 +0100

    Fixed some names

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -78,7 +78,7 @@
             <div class="row-fluid">
               <div class="box">            
                 <div class="box-header">
-                  <h2><i class="halflings-icon list-alt"></i><span class="break"></span>Things</h2>
+                  <h2><i class="halflings-icon list-alt"></i><span class="break"></span><?php echo $language['Dashboard']['Device stats'];?></h2>
                   <div class="box-icon">
                     <a href="#" class="btn-minimize"><i class="halflings-icon chevron-up"></i></a>
                     <a href="#" class="btn-close"><i class="halflings-icon remove"></i></a>
