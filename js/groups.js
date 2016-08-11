6d4bca90f33bf769c8a03b507e21c3b753c0ba29
Rudy Hardeman <zarya@gigafreak.net>
2016-08-11 22:55:32 +0200

    Synced with Halligan

--- js/groups.js
+++ js/groups.js
@@ -188,6 +188,7 @@ var groups =
   "2043" : "Zuid Nederland",
   "2044" : "Oost Nederland",
   "20441" : "Regio IJsselmond",
+  "2045" : "Nederland Techtalk",
   "2049881" : "XRF088 A",
   "2049882" : "XRF088 B",
   "2049125" : "PI2NOS",
