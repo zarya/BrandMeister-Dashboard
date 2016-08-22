32463f69ad5c84c7854c131ae67966dc01528abd
Rudy Hardeman <zarya@gigafreak.net>
2016-08-22 14:24:13 +0200

    Added Alert Radio (31079)

--- js/groups.js
+++ js/groups.js
@@ -235,6 +235,7 @@ var groups =
   "4648" : "USA - Area 8",
   "4649" : "USA - Area 9",
   "31040" : "QuadNet-BM",
+  "31079" : "Alert Radio",
   "31090" : "USA - Area 0 4640",
   "31091" : "USA - Area 1 4641",
   "31092" : "USA - Area 2 4642",
