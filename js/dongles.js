39c4632e1856a1da13d82bc0121fc4c6983ba9ab
zarya <zarya@gigafreak.net>
2016-09-25 22:09:28 +0200

    Update dongles.js

--- js/dongles.js
+++ js/dongles.js
@@ -8,7 +8,7 @@ function updateDongleListCallback(master) {
       if (value['link'] == 4) {
         var device = [];
         device[0] = CountryImage(getCountry(value['number']))+" "+value['number'];
-        device[1] = '<a href="'+baseurl+'index.php?page=lh&country=null&repeater='+value['number']+'">' + value['name'] + '</a>';
+        device[1] = '<a href="'+baseurl+'index.php?page=lh&country=null&repeater='+value['number']+'$">' + value['name'] + '</a>';
         device[2] = getRepeaterModel(value['hardware'],value['number']);
         device[3] = value['firmware'];
         device[4] = getFrequency(value['frequency1']);
